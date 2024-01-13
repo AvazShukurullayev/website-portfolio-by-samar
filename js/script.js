46a57482ee3b105bcb4bc3d437231b8197e1f956
Avaz <elonarm21@gmail.com>
2024-01-13 16:32:16 +0500

    js script added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 "use strict"
+window.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        const header = document.querySelector('header')
+        header.classList.toggle("sticky", window.scrollY > 0)
+    })
+
+    const menuBtn = document.querySelector(".nav__burger")
+    const navigation = document.querySelector(".nav__list")
+
+    menuBtn.addEventListener("click", () => {
+        menuBtn.classList.toggle("active")
+        navigation.classList.toggle("active")
+    })
+
+    const navigationItems = document.querySelectorAll(".nav__link")
+    navigationItems.forEach(navItem => {
+        navItem.addEventListener("click", () => {
+            navigation.classList.remove("active")
+            menuBtn.classList.remove("active")
+        })
+    })
+})
